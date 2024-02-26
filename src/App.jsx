5dc7b1b28d951a799d448a94538b5e46eb9e89e7
Shindeyashtech <yashshinde.tech@gmail.com>
2024-02-26 22:26:28 +0530

    created tic-tac-toe game using react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Board from './components/Board'
+// import './components/Board.css'
+function App() {
+  
+
+  return (
+    <>
+      <Board></Board> 
+    </>
+  )
+}
+
+export default App
